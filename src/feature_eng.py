cb69903f2dfc1fe9311e74a96d9cce002593c423
AnandhikaS <anandhikasaravanan@gmail.com>
2026-07-09 11:35:47 +0530

    feat: engineer price_per_sqft = price / sqft_living feature (fixes #5)

--- src/feature_eng.py
+++ src/feature_eng.py
@@ -30,11 +30,26 @@ def add_renovated_flag(df):
 if __name__ == "__main__":
 
     sample = pd.DataFrame({
-        "yr_built": [1990, 2005, 2010],
-        "yr_renovated": [0, 1998, 2012]
-    })
+    "yr_built": [1990, 2005, 2010],
+    "yr_renovated": [0, 1998, 2012],
+    "price": [400000, 550000, 620000],
+    "sqft_living": [2000, 2500, 3100]
+})
 
-    sample = add_house_age(sample)
-    sample = add_renovated_flag(sample)
+sample = add_house_age(sample)
+sample = add_renovated_flag(sample)
+sample = add_price_per_sqft(sample)
 
-    print(sample)
+print(sample)
+
+    
+def add_price_per_sqft(df):
+    """
+    Create price_per_sqft feature.
+    """
+
+    df = df.copy()
+
+    df["price_per_sqft"] = df["price"] / df["sqft_living"]
+
+    return df
